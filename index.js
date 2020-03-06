d85459fa1dd8d8bfd1d23f34dd07d25fc2e67f43
bonjourannie <artruex@gmail.com>
2020-03-06 14:39:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(source, fn){
+    let a = []
+    for (let i = 0; i < source.length; i ++){
+        let element = source[i]
+        a.push(fn(element))
+    }
+    return a;
+}
+
+function reduce(source, fn, start){
+    let a = (!!start) ? start : source[0]
+    let i = (!!start) ? 0 : 1 
+    for (; i < source.length; i++){
+        a = fn(source[i], a)
+    }
+    return a;
+}
